abe5d2429347db959b3dc911cf5b37e05a6092b4
yfajouri <yef03@hotmail.com>
2015-05-20 13:59:02 -0300

    Ejemplo web ASP
    Pagina Home y base de datos para consulta

---
+++ EjemploASP/BDService.cs
@@ -0,0 +1,44 @@
+﻿using EjemploASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploASP
+{
+    public class BDService
+    {
+         MusicStoreEntities db;
+
+         public BDService()
+         {
+             db = new MusicStoreEntities();
+         }
+
+        public  List<Album> GetAlbums()
+        {
+            return db.Albums.ToList();
+        }
+
+        public List<Artist> GetArtist()
+        {
+            return db.Artists.ToList();
+        }
+
+        public List<Genre> GetGenres()
+        {
+            return db.Genres.ToList();
+        }
+
+        public List<Album> GetAlbumsFromGenre(Genre genre)
+        {
+            return db.Albums.Where(a=>a.GenreId == genre.GenreId).ToList();
+        }
+
+        public List<Album> GetAlbumsFromIdGenre(int idGenre)
+        {
+            return db.Albums.Where(a => a.GenreId == idGenre).ToList();
+        }
+    }
+}
---
+++ WebAsp/Home.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebAsp.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="p1" runat="server">
+    
+    </div>
+    </form>
+</body>
+</html>
